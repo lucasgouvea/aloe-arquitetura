fd9216116a2627390a6da344bec741362e59abc2
Lucas Gouvea <lucasgouvea93@gmail.com>
2021-01-23 17:36:41 -0300

    materialUI e drawer navigation

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
+import React,{useState} from "react";
 import Head from "next/head";
+import { Drawer, Button  } from "@material-ui/core"
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = (drawerOpen) => {
+    setIsDrawerOpen(drawerOpen);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <React.Fragment>
+          <Button onClick={() => toggleDrawer(true)}>RIGHT</Button>
+          <Drawer anchor={'right'} open={isDrawerOpen} onClose={() =>toggleDrawer(false)}>
+            Drawer
+          </Drawer>
+      </React.Fragment>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
