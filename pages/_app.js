31e86a1f184526461bfbb79af5d0111db3ef24b4
Lucas Gouvea <lucasgouvea93@gmail.com>
2021-01-23 15:23:36 -0300

    configuracao estatica

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+Amplify.configure({ ...awsExports, ssr: true });
 import '../styles/globals.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
