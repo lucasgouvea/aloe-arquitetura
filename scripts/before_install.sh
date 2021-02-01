e2c8f8f92058e61320dacb6db3882fa252e87c3d
lgcavalho1 <lgcavalho1@latam.stefanini.com>
2021-02-01 20:24:30 -0300

    sdas

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,5 +2,6 @@ export NVM_DIR="root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+nvm use 12
 pm2 del next
 npm i
