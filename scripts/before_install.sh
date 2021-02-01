1ebb7072135c091ed31dbb3935d72e70d17cbe95
lgcavalho1 <lgcavalho1@latam.stefanini.com>
2021-02-01 20:29:09 -0300

    asjs

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,9 +2,7 @@ export NVM_DIR="root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-npm config delete prefix
-npm config set prefix $NVM_DIR/versions/node/v12.20.1
+nvm use --delete-prefix v12.20.1
 
-nvm use 12
 pm2 del next
 npm i
