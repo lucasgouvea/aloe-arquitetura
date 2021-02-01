64417714346155dadbbc1d988c5dc5c77e4e0529
lgcavalho1 <lgcavalho1@latam.stefanini.com>
2021-02-01 20:31:15 -0300

    asdas

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,1 +1,7 @@
+export NVM_DIR="root/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm use --delete-prefix v12.20.1
+
 pm2 start npm --name "next" -- run prod
