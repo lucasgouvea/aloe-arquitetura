9a7b547cb2443e2377f78b748a513e7b00252bdd
lgcavalho1 <lgcavalho1@latam.stefanini.com>
2021-02-01 20:12:36 -0300

    aws3

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-rm aloe-arquitetura.tar
-mkdir aloe-arquitetura
-cp -r .next aloe-arquitetura
-cp package.json aloe-arquitetura
-tar -cf aloe-arquitetura.tar aloe-arquitetura
-rm -rf aloe-arquitetura
