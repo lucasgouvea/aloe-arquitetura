dc455a90b3534ba38f057bdbc2f94d1de2131573
lgcavalho1 <lgcavalho1@latam.stefanini.com>
2021-02-01 21:34:07 -0300

    asdasd

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,9 @@ export NVM_DIR="root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+echo "Moving to /home/ec2-user/aloe-arquitetura"
+cd /home/ec2-user/aloe-arquitetura
+
 echo "Setting Node version - 12"
 nvm use --delete-prefix v12.20.1
 
