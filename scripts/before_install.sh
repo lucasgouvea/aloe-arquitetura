ec068c496f7b166986c246f7ea09034870d0c923
lgcavalho1 <lgcavalho1@latam.stefanini.com>
2021-02-01 21:21:10 -0300

    asd

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,14 @@
+echo "Setting NVM"
 export NVM_DIR="root/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+echo "Setting Node version - 12"
 nvm use --delete-prefix v12.20.1
 
+echo "Deleting older app version"
 pm2 del next
-npm i
+echo "Running npm install"
+npm install
+echo "Running npm run build"
 npm run build
